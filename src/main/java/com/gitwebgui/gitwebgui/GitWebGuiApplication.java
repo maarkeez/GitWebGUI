5c823abbe9ca46f9f67f9b37f6ef6237d6d7b774
David Márquez Delgado <d.marquez.delgado.91@gmail.com>
2018-09-08 00:02:00 +0200

    Initial commit

---
+++ src/main/java/com/gitwebgui/gitwebgui/GitWebGuiApplication.java
@@ -0,0 +1,12 @@
+package com.gitwebgui.gitwebgui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitWebGuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitWebGuiApplication.class, args);
+	}
+}
